08653a5bf8390eb5032d279aa05439cbb8024997
nbion1604 <falionta12@gmail.com>
2023-11-23 16:25:59 +0700

    home

--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Dimensions } from 'react-native';
 
 const Home = () => {
+
   return (
-    <View>
-      <Text style={{color:'black'}}>Home</Text>
+    <View style={styles.container1}>
+       <Image style={styles.logo}
+        source={require('../../asset/img/Simpan.png')} 
+        resizeMode="contain"
+      />
+       <Image style={styles.logo}
+        source={require('../../asset/img/Apisemangat.png')} 
+        resizeMode="contain"
+      />
+       <Image style={styles.logo}
+        source={require('../../asset/img/Puzzle.png')} 
+        resizeMode="contain"
+      />
+       <Image style={styles.logo}
+        source={require('../../asset/img/Hati.png')} 
+        resizeMode="contain"
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Home
+const styles = StyleSheet.create({
+  container1: {
+    backgroundColor: '#FFDD5B',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding:5,
+  },
+  logo: {
+    maxWidth: 40,
+    maxHeight: 40,
+    margin:20,
+    marginBottom:5
+  },
+});
 
-const styles = StyleSheet.create({})
+export default Home;
